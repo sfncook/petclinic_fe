4ed53eac7d670c827d326ecc881aeb9a8b032e15
Shawn Cook <cooksha@yahoo-inc.com>
2018-10-28 14:31:33 -0700

    PetsView

---
+++ src/components/PetsView.js
@@ -0,0 +1,133 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { withStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import {getAllPets} from "./stateHandlers/actions";
+
+const styles = theme => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: '100%',
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing.unit * 3,
+    height: '100vh',
+    overflow: 'auto',
+  },
+  tableContainer: {
+    height: 320,
+  },
+  h5: {
+    marginBottom: theme.spacing.unit * 2,
+  },
+});
+
+class PetsView_ extends Component {
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className="App">
+        <CssBaseline />
+        <div className={classes.root}>
+          <AppBar
+            position="absolute"
+          >
+            <Toolbar className={classes.toolbar}>
+              <Typography
+                component="h1"
+                variant="h6"
+                color="inherit"
+                noWrap
+                className={classes.title}
+              >
+                Pet Clinic
+              </Typography>
+              <Button color="inherit">Pets</Button>
+              <Button color="inherit">Vets</Button>
+            </Toolbar>
+          </AppBar>
+          <main className={classes.content}>
+            <div className={classes.appBarSpacer} />
+            <Typography variant="h4" gutterBottom component="h2">
+              Pets
+            </Typography>
+            <div className={classes.tableContainer}>
+              <Table className={classes.table}>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Name</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {this.props.pets.map(pet => {
+                    return (
+                      <TableRow key={pet.id}>
+                        <TableCell component="th" scope="row">
+                          {pet.name}
+                        </TableCell>
+                      </TableRow>
+                    );
+                  })}
+                </TableBody>
+              </Table>
+            </div>
+          </main>
+        </div>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    this.props.getAllPets();
+  }
+
+}
+
+const mapStateToProps = state => {
+  return {
+    pets: state.pets,
+  }
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getAllPets: () => {
+      dispatch(getAllPets());
+    }
+  }
+};
+
+const PetsView = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PetsView_);
+
+// export default App;
+export default withStyles(styles)(PetsView);
