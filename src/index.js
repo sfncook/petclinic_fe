5de5cef321690a4b0f90a41c234af28345c60986
Shawn Cook <cooksha@yahoo-inc.com>
2018-10-28 13:58:33 -0700

    apply thunk and logger middleware

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
+import thunkMiddleware from 'redux-thunk'
+import { createLogger } from 'redux-logger'
 import React from 'react';
 import { render } from 'react-dom'
 import './styles/index.css';
 import App from './App';
 
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import petClinicReducer from './stateHandlers/reducers';
 
-const store = createStore(petClinicReducer);
+const loggerMiddleware = createLogger();
+
+const store = createStore(petClinicReducer,
+  applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware
+  ));
 
 render(
   <Provider store={store}>
