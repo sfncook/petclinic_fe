32913dc251d70be6fa3b776ac09ac54ad14ae092
Shawn Cook <cooksha@yahoo-inc.com>
2018-10-28 14:40:21 -0700

    Init add router

---
+++ src/components/Foo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Foo extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <h1>FOO</h1>
+      </div>
+    );
+  }
+
+}
+
+export default Foo;
--- src/components/PetsView.js
+++ src/components/PetsView.js
@@ -11,7 +11,7 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import {getAllPets} from "./stateHandlers/actions";
+import {getAllPets} from "../stateHandlers/actions";
 
 const styles = theme => ({
   root: {
